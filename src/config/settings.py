4fee690b7d6661fe623404efae3f145c15be44c5
Amit Yadav <amityadav4664@gmail.com>
2025-05-25 17:33:38 +0530

    improvements

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pathlib import Path
 from pydantic_settings import BaseSettings
 
@@ -7,12 +8,13 @@ class AppConfig(BaseSettings):
     WRK_DIR: Path = Path(__file__).resolve().parents[1]
 
     ONLINE_SIM_APIKEY: str
-    NOPECHA_API_KEY: str
 
     OXYLABS_USERNAME: str
     OXYLABS_PASSWORD: str
     OXYLABS_IP: str = "pr.oxylabs.io"
     OXYLABS_PORT: int = 7777
+
+    NOPECHA_API_KEY: Optional[str]
 
     class Config:
         env_file = "config/.env"
